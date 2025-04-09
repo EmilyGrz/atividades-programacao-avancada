76304474b404dd6eea688252492501b4d87890a0
EmilyGrz <emillygrzgorczyki@gmail.com>
2025-04-09 16:26:13 -0300

    Update Main.java

--- src/exercicioBanco/Main.java
+++ src/exercicioBanco/Main.java
@@ -5,12 +5,16 @@ public class Main {
         ContaCorrente cc = new ContaCorrente("João Silva", "1234", "56789-0", 1000, 5);
         ContaPoupanca cp = new ContaPoupanca("Maria Souza", "4321", "98765-0", 2000, 0.5);
 
-        cc.depositar(500);
-        cc.sacar(200);
-        System.out.println("Imposto de Renda da Conta Corrente: R$ " + cc.calcularIR());
-        
-        cp.depositar(1000);
-        cp.aplicarRendimento();
-        cp.sacar(500);
+        try {
+            cc.depositar(500);
+            cc.sacar(200);
+            System.out.println("Imposto de Renda da Conta Corrente: R$ " + cc.calcularIR());
+
+            cp.depositar(1000);
+            cp.aplicarRendimento();
+            cp.sacar(500);
+        } catch (SaldoInsuficienteException e) {
+            System.out.println("Erro ao sacar: " + e.getMessage());
+        }
     }
 }
